17c04c9296bd42b21a96f4e3d6f3ec0ed4da6b4a
Marco Castro <marcodotcastro@gmail.com>
2024-10-07 10:39:04 -0300

    sample project ruby
    (cherry picked from commit 0882b337da11085bf85eb446cdc932fa6e38b222)

---
+++ saude_ruby/lib/saude_ruby/aluno.rb
@@ -0,0 +1,31 @@
+# lib/saude_ruby/aluno.rb
+module SaudeRuby
+  class Aluno
+    attr_reader :peso, :altura, :idade, :sexo, :cintura, :quadril
+
+    def initialize(peso:, altura:, idade:, sexo:, cintura:, quadril:)
+      @peso = peso
+      @altura = altura
+      @idade = idade
+      @sexo = sexo
+      @cintura = cintura
+      @quadril = quadril
+    end
+
+    def imc
+      IMC.calcular(@peso, @altura)
+    end
+
+    def tmb
+      TMB.calcular(@peso, @altura * 100, @idade, @sexo) # altura convertida para cm
+    end
+
+    def rcq
+      RCQ.calcular(@cintura, @quadril)
+    end
+
+    def pg
+      PG.calcular(imc, @idade, @sexo)
+    end
+  end
+end
